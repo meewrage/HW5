2ee7ad160685e8cd02954c57f09f75a2f55f3bc6
meewrage <luki_wop@mail.ru>
2023-02-28 21:25:49 +0300

    added task32

---
+++ task32/Program.cs
@@ -0,0 +1,39 @@
+﻿//Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+//[-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+int ReadNumber(string messageToUser)
+{
+    Console.WriteLine(messageToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+
+int[] GetRandomArray(int length, int leftRange, int rightRange)
+{
+
+    int[] array = new int[length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Random.Shared.Next(leftRange, rightRange + 1);
+    }
+
+    return array;
+}
+
+int [] ReverseArray(int[] array)
+{
+    for( int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+    }
+    return array;
+}
+int length = ReadNumber("Введите длину массива");
+int leftRange = ReadNumber("Введите правую границу");
+int rightRange = ReadNumber("Введите левую границу");
+int[] array = GetRandomArray(length,leftRange,rightRange);
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine($"[{string.Join(", ", ReverseArray(array))}]");
